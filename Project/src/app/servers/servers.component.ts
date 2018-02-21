6d27dd90dbdb412d9b04ae0c6ce9b88c28941f76
ajai1 <emailtoajai@gmail.com>
2018-02-21 21:14:09 +0530

    DataBinding Concepts

--- Project/src/app/servers/servers.component.ts
+++ Project/src/app/servers/servers.component.ts
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  serverStatus = 'No Server Created';
+  constructor() {
+    setTimeout(()=>{
+        this.allowNewServer = true;
+    },2000);
+  }
 
   ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverStatus = 'Server Created...!';
   }
 
 }
